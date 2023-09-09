2f34d92b0e1fc6820b2e24469f1536500d2e973f
Brandon James <brandontjames2@gmail.com>
2023-09-09 16:15:06 -0400

    getting rid of demo app stuff

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -16,18 +16,11 @@
       <path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
     </svg>
     <ul>
-      <li aria-current={$page.url.pathname === "/" ? "page" : undefined}>
+      <li>
         <a href="/">Home</a>
       </li>
-      <li aria-current={$page.url.pathname === "/about" ? "page" : undefined}>
-        <a href="/about">About</a>
-      </li>
-      <li
-        aria-current={$page.url.pathname.startsWith("/sverdle")
-          ? "page"
-          : undefined}
-      >
-        <a href="/sverdle">Sverdle</a>
+      <li>
+        <a href="/test">Test</a>
       </li>
     </ul>
     <svg viewBox="0 0 2 3" aria-hidden="true">
@@ -99,18 +92,6 @@
   li {
     position: relative;
     height: 100%;
-  }
-
-  li[aria-current="page"]::before {
-    --size: 6px;
-    content: "";
-    width: 0;
-    height: 0;
-    position: absolute;
-    top: 0;
-    left: calc(50% - var(--size));
-    border: var(--size) solid transparent;
-    border-top: var(--size) solid var(--color-theme-1);
   }
 
   nav a {
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<div>
+  <p>This is our new page.</p>
+</div>
+
+<style>
+</style>
