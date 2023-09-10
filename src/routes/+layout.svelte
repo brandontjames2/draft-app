73e943d0274fb211ed51fef7a30cfa9b7a973529
Brandon James <brandontjames2@gmail.com>
2023-09-10 16:33:37 -0400

    getting rid of sass and default app stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,9 @@
 <script>
   import "../app.postcss";
   import Header from "./Header.svelte";
-  import "./styles.scss";
 </script>
 
-<div class="app">
+<div>
   <Header></Header>
 
   <main>
@@ -18,40 +17,5 @@
   </footer>
 </div>
 
-<style lang="scss">
-  @use "./styles";
-  .app {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-  }
-
-  main {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    padding: 1rem;
-    width: 100%;
-    max-width: 64rem;
-    margin: 0 auto;
-    box-sizing: border-box;
-  }
-
-  footer {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 12px;
-    // a {
-    //   font-weight: bold;
-    //   color: styles.$blue;
-    // }
-  }
-
-  @media (min-width: 480px) {
-    footer {
-      padding: 12px 0;
-    }
-  }
+<style>
 </style>
